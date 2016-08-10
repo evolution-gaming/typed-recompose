bd37c597f51c0c572907c1c592fe393314434a56
Pavel Birukov <birukov.pavel@gmail.com>
2016-08-10 19:14:08 +0300

    withContext dump annotation

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,8 @@
 declare module "recompose" {
     import * as React from 'react';
-    type HocToHoc<T> = (cmp: T) => T;
-    export function withContext<P, T>(
-        childContextTypes: React.ValidationMap<P>,
-        getChildContext: (props: P) => any
-    ): HocToHoc<T>;
+    export type HigherOrderComponent<T> = (cmp: T) => T;
 
-    export function withContext(): void;
+    export function withContext<ContextProps, ComponentOwnProps, Component>(
+        childContextTypes: React.ValidationMap<ContextProps>,
+        getChildContext: (props: ComponentOwnProps) => any): HigherOrderComponent<Component>;
 }
