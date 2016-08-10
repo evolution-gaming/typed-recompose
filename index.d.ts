2ce12db3b37038fb0f28a8e6763d042a5c5887ec
Pavel Birukov <birukov.pavel@gmail.com>
2016-08-10 15:47:54 +0300

    annotation added

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,10 @@
-export function withContext(): void;
+declare module "recompose" {
+    import * as React from 'react';
+    type HocToHoc<T> = (cmp: T) => T;
+    export function withContext<P, T>(
+        childContextTypes: React.ValidationMap<P>,
+        getChildContext: (props: P) => any
+    ): HocToHoc<T>;
+
+    export function withContext(): void;
+}
