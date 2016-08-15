8e4ba66bc920f6046e3fd6661fe660b33948d9b5
Pavel Birukov <birukov.pavel@gmail.com>
2016-08-15 18:30:49 +0300

    import changed

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,15 @@
+import { ComponentClass, StatelessComponent, ValidationMap } from 'react';
+
 declare module "recompose" {
-    import * as React from 'react';
     export interface HigherOrderComponent {
         // stolen from react-redux
         // export interface InferableComponentDecorator {
         //     <P, TComponentConstruct extends (ComponentClass<P>|StatelessComponent<P>)>(component: TComponentConstruct): TComponentConstruct;
         // }
-        <P, TComponentConstruct extends (React.ComponentClass<P>|React.StatelessComponent<P>)>(component: TComponentConstruct): TComponentConstruct;
+        <P, TComponentConstruct extends (ComponentClass<P>|StatelessComponent<P>)>(component: TComponentConstruct): TComponentConstruct;
     }
 
     export function withContext<ContextProps, ComponentOwnProps>(
-        childContextTypes: React.ValidationMap<ContextProps>,
+        childContextTypes: ValidationMap<ContextProps>,
         getChildContext: (props: ComponentOwnProps) => any): HigherOrderComponent;
 }
