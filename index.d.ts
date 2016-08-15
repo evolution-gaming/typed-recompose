5de5e3cbdef347c3ba6c85e6c15b31b9229ef3a5
Pavel Birukov <birukov.pavel@gmail.com>
2016-08-15 18:24:35 +0300

    HigherOrderComponent from react-redux added

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,14 @@
 declare module "recompose" {
     import * as React from 'react';
-    export type HigherOrderComponent<T> = (cmp: T) => T;
+    export interface HigherOrderComponent {
+        // stolen from react-redux
+        // export interface InferableComponentDecorator {
+        //     <P, TComponentConstruct extends (ComponentClass<P>|StatelessComponent<P>)>(component: TComponentConstruct): TComponentConstruct;
+        // }
+        <P, TComponentConstruct extends (React.ComponentClass<P>|React.StatelessComponent<P>)>(component: TComponentConstruct): TComponentConstruct;
+    }
 
-    export function withContext<ContextProps, ComponentOwnProps, Component>(
+    export function withContext<ContextProps, ComponentOwnProps>(
         childContextTypes: React.ValidationMap<ContextProps>,
-        getChildContext: (props: ComponentOwnProps) => any): HigherOrderComponent<Component>;
+        getChildContext: (props: ComponentOwnProps) => any): HigherOrderComponent;
 }
